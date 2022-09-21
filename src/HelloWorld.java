0c55182ed9b92d30bcc6100a7d14909ec603a4df
1E10 Laptop <sample@bths.edu>
2022-09-21 10:47:53 -0400

    changed a line of code

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello what is your name?");
+        System.out.println("Wow you're so cool!");
     }
 }
 
